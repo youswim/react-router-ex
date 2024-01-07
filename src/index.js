024bf43dd9411e4fbd18f6303a90c78485706065
youswim <youswim96@gmail.com>
2024-01-07 22:21:29 +0900

    Revert "공식 문서에서 알려준 방법으로 라우팅"
    This reverts commit b83f580d58af80939224f6e320b6f7e9fb99a526.
    
    강의와 거의 동일한 코드로 원복.
    학습 중의 혼동을 피하기 위함.

--- src/index.js
+++ src/index.js
@@ -3,30 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Home from './Home';
-import Cat from './Cat';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home/>
-  },
-  {
-    path: "cat/:cat_name",
-    element: <Cat/>
-  }
-])
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-    <RouterProvider router={router} />
-
-    // <BrowserRouter>
-    //   <App/>
-    // </BrowserRouter>
-
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
