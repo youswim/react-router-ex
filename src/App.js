a19510250bee506ce801d3e6a33231288b828c7a
youswim <youswim96@gmail.com>
2024-01-07 22:35:12 +0900

    Link 태그 사용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, useParams } from "react-router-dom";
+import { Routes, Route, useParams, Link } from "react-router-dom";
 
 import Home from "./Home";
 import Dog from "./Dog";
@@ -14,6 +14,13 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+
+        <div>
+          <Link to="/">Home으로 가기</Link>
+          <Link to="/cat/nabi">Cat으로 가기</Link>
+          <Link to="/dog">Dog으로 가기</Link>
+        </div>
+
         <Routes>
           <Route exact path="/" element={<Home/>} />
           <Route path="/cat" element={<Cat/>} />
