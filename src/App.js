5cf3cf47d542a8f4421bb6eb09c1b16808c6ac50
youswim <youswim96@gmail.com>
2024-01-07 23:10:20 +0900

    useNavigate 사용을 위해 App.js를 함수형 컴포넌트로 변경

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
 import React from "react";
-import { Routes, Route, useParams, Link } from "react-router-dom";
+import { Routes, Route, useParams, Link, useNavigate } from "react-router-dom";
 
 import Home from "./Home";
 import Dog from "./Dog";
 import Cat from "./Cat";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const App = () => {
+  return (
+    <div className="App">
 
-  render() {
-    return (
-      <div className="App">
-
-        <div>
-          <Link to="/">Home으로 가기</Link>
-          <Link to="/cat/nabi">Cat으로 가기</Link>
-          <Link to="/dog">Dog으로 가기</Link>
-        </div>
-
-        <Routes>
-          <Route exact path="/" element={<Home/>} />
-          <Route path="/cat" element={<Cat/>} />
-          <Route path="/cat/:cat_name" element={<Cat/>} />
-          <Route path="/dog" element={<Dog/>} />
-        </Routes>
+      <div>
+        <Link to="/">Home으로 가기</Link>
+        <Link to="/cat/nabi">Cat으로 가기</Link>
+        <Link to="/dog">Dog으로 가기</Link>
       </div>
-    );
-  }
+
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route path="/cat" element={<Cat/>} />
+        <Route path="/cat/:cat_name" element={<Cat/>} />
+        <Route path="/dog" element={<Dog/>} />
+      </Routes>
+      <button>이전으로 가기</button>
+    </div>
+  );
 }
 
 export default App;
